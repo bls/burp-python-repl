a6499d5d26c4969ab9ba7cea91e8e63c8957b84b
Blair Strang <Blair.Strang@auspost.com.au>
2015-09-01 15:45:50 +1000

    Oops, commit built plugin corresponding with src.

--- bin/BurpPythonRepl.py
+++ bin/BurpPythonRepl.py
@@ -30,33 +30,40 @@ sys.meta_path = [Importer()]
 
 # -----------------------------------------
 
-from burp import IBurpExtender, IExtensionStateListener
-import repl
+import sys
+import os
 import logging
+from burp import IBurpExtender, IExtensionStateListener
+
 
 log = logging.getLogger(__package__)
 
 class BurpExtender(IBurpExtender, IExtensionStateListener):
     def __init__(self):
-        self.repl = repl.Repl()
+        self.repl = None
         self.callbacks = None
     
     def registerExtenderCallbacks(self, callbacks):
         self.callbacks = callbacks
         self.callbacks.setExtensionName("BurpPythonRepl")
         self.callbacks.registerExtensionStateListener(self)
+        self.setImportPath()
         self.configureLogging()
-        repl.locals_dir['cb'] = self.callbacks
-        repl.banner = self.getConsoleBanner()
-        self.repl.run()
+        self.startRepl()
 
     def extensionUnloaded(self):
-        self.repl.shutdown()
+        self.stopRepl()
 
     def configureLogging(self):
         burp_stdout = self.callbacks.getStdout()
         logging.basicConfig(level=logging.DEBUG, stream=burp_stdout)
-
+        
+    def setImportPath(self):
+        scriptFile = self.callbacks.getExtensionFilename()
+        scriptDir = os.path.dirname(os.path.abspath(scriptFile))
+        if scriptDir not in sys.path:
+            sys.path.insert(0, scriptDir)
+        
     def getConsoleBanner(self):
         burpVer = self.callbacks.getBurpVersion()
         return '\n'.join([
@@ -65,5 +72,16 @@ class BurpExtender(IBurpExtender, IExtensionStateListener):
             'Product: {0} {1}.{2}'.format(burpVer[0], burpVer[1], burpVer[2]),
             'Type "help", "copyright", "credits" or "license" for more information.'
         ])
+
+    def startRepl(self):
+        import repl
+        self.repl = repl.Repl()
+        repl.locals_dir['cb'] = self.callbacks
+        repl.banner = self.getConsoleBanner()
+        self.repl.run()
+
+    def stopRepl(self):
+        self.repl.shutdown()
+
 
 
